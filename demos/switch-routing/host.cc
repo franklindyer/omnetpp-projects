191546440df071940f20281d91ca40246b747971
Franklin <franklin@dyer.me>
2023-10-02 00:37:31 +0000

    collect correct/incorrect packet stats in switch routing demo

--- demos/switch-routing/host.cc
+++ demos/switch-routing/host.cc
@@ -9,6 +9,9 @@ using namespace omnetpp;
 class Host : public cSimpleModule {
     private:
         cMessage *event;
+        int numSent;
+        int numReceived;
+        int numRefused;
     public:
         virtual ~Host();
     protected:
@@ -16,6 +19,7 @@ class Host : public cSimpleModule {
         virtual void scheduleMessage();
         virtual void generateMessage();
         virtual void handleMessage(cMessage *msg);
+        virtual void finish();
 };
 
 Define_Module(Host);
@@ -48,6 +52,7 @@ void Host::generateMessage() {
     msg->setSrc(src);
     msg->setDst(dst);
     send(msg->dup(), "out");
+    numSent++;
     delete msg;
 
     EV_INFO << "Host " << src << " sends message to Host " << dst << "\n";
@@ -58,7 +63,21 @@ void Host::handleMessage(cMessage *msg) {
         generateMessage();
         scheduleMessage();
     } else {
-        EV_INFO << "Host " << getIndex() << " receives message: '" << msg->getName() << "'\n";
-        delete msg;
+        HostMessage *hMsg = (HostMessage*)msg;
+        if (hMsg->getDst() == getIndex()) {
+            EV_INFO << "Host " << getIndex() << " receives message: '" << msg->getName() << "'\n";
+            numReceived++;
+            delete msg;
+        } else {
+            EV_INFO << "Host " << getIndex() << " refuses a message addressed for someone else\n";
+            numRefused++;
+            delete msg;
+        }
     }
+}
+
+void Host::finish() {
+    recordScalar("#sent", numSent);
+    recordScalar("#received-correct", numReceived);
+    recordScalar("#received-incorrect", numRefused);
 }
